68ccb9d85a77599e85d8eb52de84fdfceb7c1bb4
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-01-13 16:18:48 +0300

    build panels

---
+++ src/ui/components/panel/TerrainPanel.java
@@ -0,0 +1,24 @@
+package ui.components.panel;
+import javax.swing.*;
+
+import foot.Terrain;
+
+public class TerrainPanel extends JPanel {
+    private JLabel sizeLabel;
+    private JLabel orientationLabel;
+
+    public TerrainPanel() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        sizeLabel = new JLabel("Size: ");
+        orientationLabel = new JLabel("Orientation: ");
+        add(sizeLabel);
+        add(orientationLabel);
+    }
+
+    public void updateDetails(Terrain terrain) {
+        if (terrain != null) {
+            sizeLabel.setText("Size: " + terrain.getSize().width + " x " + terrain.getSize().height);
+            orientationLabel.setText("Orientation: " + (terrain.isVertical() ? "Vertical" : "Horizontal"));
+        }
+    }
+}
---
+++ src/ui/foot/TerrainUI.java
@@ -0,0 +1,90 @@
+package ui.foot;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+
+import foot.Terrain;
+import interfaces.DrawableComponent;
+
+public class TerrainUI implements DrawableComponent{
+    Terrain terrain;
+    Image image;
+    Point coordonnee;
+    Rectangle drawAspect;
+    Rectangle containerAspect;
+    public TerrainUI(Terrain terrain,int containerWidth, int containerHeight){
+        setTerrain(terrain);
+        setImage(getTerrain().getImage());
+        setContainerAspect(new Rectangle(containerWidth, containerHeight));
+
+        setDrawAspect(buildDrawAspect());
+        setCoordonnee(buildCoordonnee());
+    }
+    @Override
+    public void draw(Graphics g) {
+        g.drawImage(this.getImage(), (int)getCoordonnee().getX(), (int)getCoordonnee().getY(), null);
+    }
+
+    public void setTerrain(Terrain terrain) {
+        this.terrain = terrain;
+    }
+    public Terrain getTerrain() {
+        return terrain;
+    }
+
+    public void setDrawAspect(Rectangle drawAspect) {
+        this.drawAspect = drawAspect;
+    }
+    public Rectangle getDrawAspect() {
+        if (drawAspect == null) {
+            setDrawAspect(buildDrawAspect());
+        }
+        return drawAspect;
+    }
+
+    private Rectangle buildDrawAspect(){
+        int imgWidth = getImage().getWidth(null);
+        int imgHeight = getImage().getHeight(null);
+        double imgAspect = (double) imgWidth / imgHeight;
+        double containerAspect = (double) getContainerAspect().getWidth() / getContainerAspect().getHeight();
+
+        int drawWidth, drawHeight;
+        if (imgAspect > containerAspect) {
+            drawWidth = (int)getContainerAspect().getWidth();
+            drawHeight = (int) (getContainerAspect().getWidth() / imgAspect);
+        } else {
+            drawHeight = (int)getContainerAspect().getHeight();
+            drawWidth = (int) (getContainerAspect().getHeight() * imgAspect);
+        }
+        return new Rectangle(drawWidth,drawHeight);
+    }
+
+    private Point buildCoordonnee(){
+        int x = ((int)getContainerAspect().getWidth() - (int)getDrawAspect().getWidth())/2;
+
+        int y = ((int)getContainerAspect().getHeight() - (int)getDrawAspect().getWidth())/2; 
+
+        return new Point(x, y);
+    }
+
+    public Point getCoordonnee() {
+        return coordonnee;
+    }
+    public void setCoordonnee(Point coordonnee) {
+        this.coordonnee = coordonnee;
+    }
+    public Rectangle getContainerAspect() {
+        return containerAspect;
+    }
+    public void setContainerAspect(Rectangle containerAspect) {
+        this.containerAspect = containerAspect;
+    }
+    public Image getImage() {
+        return image;
+    }
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
